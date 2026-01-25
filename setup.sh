39c8c6758fc432ea0026787b66c188cd1328677c
bczsalba <bczsalba@gmail.com>
2026-01-25 18:58:52 +0100

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+ln -sf "$HOME/.config/bash/bashrc" "$HOME/.bashrc"
+ln -sf "$HOME/.config/bash/bash_profile" "$HOME/.bash_profile"
+ln -sf "$HOME/.config/vim/vimrc" "$HOME/.vimrc"
