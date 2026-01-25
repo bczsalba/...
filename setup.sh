ad6eab0b2c28b121715debe47f726e8880758d56
bczsalba <bczsalba@gmail.com>
2026-01-25 19:04:23 +0100

    Remove .swap and .undo files and add them to setup.sh instead

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+mkdir $HOME/.config/vim/.{swap,undo}
 ln -sf "$HOME/.config/bash/bashrc" "$HOME/.bashrc"
 ln -sf "$HOME/.config/bash/bash_profile" "$HOME/.bash_profile"
 ln -sf "$HOME/.config/vim/vimrc" "$HOME/.vimrc"
