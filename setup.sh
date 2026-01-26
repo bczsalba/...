5744e9779045b3599b57d3d63521ba965a9ff503
bczsalba <bczsalba@gmail.com>
2026-01-26 16:31:42 +0100

    Link .vim -> .config/vim

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ mkdir $HOME/.config/vim/.{swap,undo}
 ln -sf "$HOME/.config/bash/bashrc" "$HOME/.bashrc"
 ln -sf "$HOME/.config/bash/bash_profile" "$HOME/.bash_profile"
 ln -sf "$HOME/.config/vim/vimrc" "$HOME/.vimrc"
+ln -sf "$HOME/.config/vim" "$HOME/.vim"
